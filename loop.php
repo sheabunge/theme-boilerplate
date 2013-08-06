5cff7b26cc20a8762b5ce9fff1adc8fc39456f66
Shea Bunge <info@bungeshea.com>
2013-08-06 15:33:18 +1000

    Update Hybrid Core to 1.6

--- loop.php
+++ loop.php
@@ -15,8 +15,7 @@ if ( have_posts() ) {
 	while ( have_posts() ) {
 		the_post();
 
-		/* Update when Hybrid Core 1.6 is released. This function will no longer be available. */
-		boilerplate_get_content_template();
+		hybrid_get_content_template();
 
 		if ( is_singular() ) {
 			comments_template();
